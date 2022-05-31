e6e037a862cd4870dcc62cf7e167efee5bd0b4d5
OhmPatil <workpatil10@gmail.com>
2022-05-31 12:40:34 +0530

    Change reset button logic

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const board = document.querySelector('.board')
 
-createBoard(document.querySelector('#size').value)
+// createBoard(document.querySelector('#size').value)
+// createBoard(16)
 
 function createBoard(size){
+    let squares = board.querySelectorAll('div')
+    squares.forEach(square => square.remove())
+    
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`
     
     let total = size*size;
     for (let i=0; i<total; i++){
         let pixel = document.createElement('div')
-        pixel.style.background = 'white'
+        pixel.style.background = 'red'
+        pixel.addEventListener('mouseover', ()=>{
+            pixel.style.backgroundColor = 'black';
+        })
         pixel.classList.add('pixel')
         pixel.classList.add('pixelhover')
         board.appendChild(pixel)
@@ -24,14 +31,8 @@ function changeSize(){
 }
 changeSize() //Watch for changes in grid size change form
 
-const pixels = document.querySelectorAll('.pixel')
-
-// logic when 'black' button is pressed
-function black(){
-    pixels.forEach(pixel => pixel.addEventListener('mouseover', () => pixel.setAttribute('style', 'background:black;')))
-}
 
 // logic when 'reset' button is pressed
 function reset(){
-    pixels.forEach(pixel => pixel.setAttribute('style', 'background:white'))
+    createBoard(document.querySelector('#size').value)
 }
